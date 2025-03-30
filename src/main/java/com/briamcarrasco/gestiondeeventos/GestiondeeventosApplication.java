4dda2caed107abf4b4d06017180a4e66992a0d59
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-03-30 01:59:04 -0300

    Archivos iniciales

---
+++ src/main/java/com/briamcarrasco/gestiondeeventos/GestiondeeventosApplication.java
@@ -0,0 +1,13 @@
+package com.briamcarrasco.gestiondeeventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestiondeeventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestiondeeventosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/briamcarrasco/gestiondeeventos/GestiondeeventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.briamcarrasco.gestiondeeventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestiondeeventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
